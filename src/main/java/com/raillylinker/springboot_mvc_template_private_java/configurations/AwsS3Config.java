610b951dde9b2ddebdc02cab5a2858490150a5f3
RaillyLinker <raillylinker@gmail.com>
2024-10-30 19:10:43 +0900

    feat : AWS Configuration class 추가

---
+++ src/main/java/com/raillylinker/springboot_mvc_template_private_java/configurations/AwsS3Config.java
@@ -0,0 +1,34 @@
+package com.raillylinker.springboot_mvc_template_private_java.configurations;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// [AWS S3 설정]
+@Configuration
+public class AwsS3Config {
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public @Valid @NotNull @org.jetbrains.annotations.NotNull AmazonS3 amazonS3Client() {
+        @Valid @NotNull @org.jetbrains.annotations.NotNull
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
+                .build();
+    }
+}
